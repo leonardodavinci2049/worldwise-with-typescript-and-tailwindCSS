f0c235c073c8941df7fd675c2f03b811255e0500
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-03-18 18:55:33 -0300

    start project

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-3xl bg-red-500 text-white font-bold">New Project React</h1>
+  </div>;
+};
+
+export default App;
